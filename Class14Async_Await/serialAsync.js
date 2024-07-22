869253e0897c5c1e8c816f86fbec68eccc05012b
premharry96 <premharry96@gmail.com>
2024-07-23 01:49:48 +0530

    new updates

--- Class14Async_Await/serialAsync.js
+++ Class14Async_Await/serialAsync.js
@@ -1,18 +1,18 @@
-const { reject } = require("async");
+//const { reject } = require("async");
 
 const placeOrder = (order) => {
   return new Promise((resolve, reject) => {
     if (order === "coffee") {
-      resolve("Served coffee");
+      resolve("Coffee order placed");
     } else {
-      reject("cannot serve coffee");
+      reject("cannot place coffee order");
     }
   });
 };
 
 const orderProcessed = (orderId) => {
   return new Promise((resolve, reject) => {
-    resolve(`${orderId} processed`);
+    resolve(`${orderId} processed and served`);
   });
 };
 
@@ -22,14 +22,26 @@ const generateBill = (procOrder) => {
   });
 };
 
-placeOrder("coffee")
-  .then((data) => {
-    console.log(data);
-    return orderProcessed("001");
-  })
-  .catch((error) => console.log(error))
-  .then((Id) => {
-    console.log(Id);
-    return generateBill("001");
-  })
-  .then((bill) => console.log(bill));
+const handlingOrder = async () => {
+  let isOrderPlaced = placeOrder("coffee");
+  let isPromiseHandled = await isOrderPlaced;
+  console.log(isPromiseHandled);
+  let orderPromise = await orderProcessed("001 - coffee");
+  console.log(orderPromise);
+  let billPromise = await generateBill("001 - coffee");
+  console.log(billPromise);
+};
+
+handlingOrder();
+
+// placeOrder("coffe")
+//   .then((data) => {
+//     console.log(data);
+//     return orderProcessed("001");
+//   })
+//   .then((Id) => {
+//     console.log(Id);
+//     return generateBill("001");
+//   })
+//   .then((bill) => console.log(bill))
+//   .catch((error) => console.log(error));
